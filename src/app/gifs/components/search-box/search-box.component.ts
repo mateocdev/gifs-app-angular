ecfa36650faa4d7cee8800d7627b7e856c7a3155
Mateo Castano <m.castano1@uniandes.edu.co>
2024-02-25 01:10:52 -0500

    :construction: Adding decorator viewchild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css',
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  @ViewChild('searchInput')
+  public searchInput!: ElementRef<HTMLInputElement>;
+
+  public searchGifs(): void {
+    const newSearch = this.searchInput.nativeElement.value;
+    console.log(newSearch);
+  }
+}
