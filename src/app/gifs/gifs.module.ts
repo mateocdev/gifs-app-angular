6913dfe4426b6af11287604b42f370060b3ca71c
Mateo Castano <m.castano1@uniandes.edu.co>
2024-02-25 18:00:38 -0500

    :construction: Wip gifs page

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
   imports: [CommonModule],
   exports: [HomePageComponent],
 })
