82e1644e443f2c6d4734bd0326a32a11c62a08bb
Mateo Castano <m.castano1@uniandes.edu.co>
2024-03-03 11:27:45 -0500

    :sparkles: Lazy image

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-card-list',
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { HomePageComponent } from './pages/home/home-page.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
-  imports: [CommonModule],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+    GifsCardComponent,
+  ],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
