7f07f49316e8b2b24eb33b10d9eb3b6568a3f0ff
Mateo Castano <m.castano1@uniandes.edu.co>
2024-03-03 11:59:28 -0500

    :sparkles: Lazy image

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-image',
   templateUrl: './lazy-image.component.html',
-  styleUrl: './lazy-image.component.css'
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+  public hasLoaded: boolean = false;
+  ngOnInit(): void {
+    if (!this.url) {
+      throw new Error('Attribute url is required');
+    }
+  }
+  @Input()
+  public url!: string;
 
+  @Input()
+  public alt!: string;
+
+  onLoad() {
+    this.hasLoaded = true;
+  }
 }
